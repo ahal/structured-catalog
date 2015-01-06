b1335843ca94a71d63426c57ce0119bb542711a8
Andrew Halberstadt <halbersa@gmail.com>
2015-01-06 13:54:20 -0500

    Encode/decode dots in blobber_files keys

--- catalog/queue/mongo.py
+++ catalog/queue/mongo.py
@@ -10,6 +10,7 @@ def do_delayed_imports():
 class MongoQueue(BaseQueue):
     _lock = Lock()
     processed_count = None
+    dot_encoding = '\x95\x25\xab\x6e'
 
     def __init__(self):
         do_delayed_imports()
@@ -17,7 +18,20 @@ class MongoQueue(BaseQueue):
         self.client = pymongo.MongoClient('localhost', 27012)
         self.db = self.client.structured_catalog
 
+    def encode_blobber_file_keys(job):
+        blobber_files = {}
+        for k, v in job['blobber_files'].iteritems():
+            blobber_files[k.replace('.', self.dot_encoding)] = v
+        job['blobber_files'] = blobber_files
+
+    def decode_blobber_file_keys(job):
+        blobber_files = {}
+        for k, v in job['blobber_files'].iteritems():
+            blobber_files[k.replace(self.dot_encoding, '.')] = v
+        job['blobber_files'] = blobber_files
+
     def push(self, job):
+        self.encode_blobber_file_keys(job)
         job = {
             'payload': job,
             'processed_count': 0,
@@ -38,7 +52,9 @@ class MongoQueue(BaseQueue):
 
             job['processed_count'] += 1
             self.db.jobs.save(job)
-            return job['payload']
+            job = job['payload']
+            self.decode_blobber_file_keys(job)
+            return job
         finally:
             self._lock.release()
 
